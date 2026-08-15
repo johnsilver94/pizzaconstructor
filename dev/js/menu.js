19d9ec0ff99f901b336732be7a0ba1370c378567
Ion Cretu <cretuion94@gmail.com>
2026-08-15 18:58:09 +0300

    fix(ui): fix page layout wrappers and responsive grid alignment (#34)

--- dev/js/menu.js
+++ dev/js/menu.js
@@ -1,13 +1,7 @@
 /* eslint-disable no-undef */
 $(function() {
   $("input[name='category']").on('change', function(e) {
-    e.preventDefault();
-    var category = $(this)
-      .parent()
-      .parent()
-      .find("input[name='category']:checked")
-      .val();
-
-    console.log(category);
+    var category = $(this).val();
+    window.location.href = '/menu/' + category;
   });
 });
